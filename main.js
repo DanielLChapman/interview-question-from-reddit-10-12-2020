47add064100f587a9f879f990125904facae921a
Daniel Chapman <danielc@my.ccsu.edu>
2020-10-12 16:23:38 -0400

    Json To Tree

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 let json = {};
+class TreeNode2 {
+    constructor(name, data) {
+        this.name = name;
+        this.data = data;
+        this.descendents = {};
+    }
+}
+
+let root = new TreeNode2();
 
 //grabs the url from the input
 async function grabUrl() {
@@ -17,30 +26,38 @@ async function grabUrl() {
                 //convert to json and move onto next function
                 response.json().then(function(data) {
                     json = data;
-                    convertData(json);
+                    convertData(root, json);
                 });
             }
         )
         .catch(function(err) {
-            console.log('Fetch Error :-S', err);
+            console.log('Fetch Error', err);
         });
 }
 
-function convertData(data) {
-    console.log(data);
+function convertData(currentNode, data) {
+    //loop over current data on the current node.
+    Object.keys(data).forEach((x) => {
+        //if the data string/number, can make an empty node
+        if (typeof data[x] !== 'object') {
+            currentNode.descendents[x] = new TreeNode2(x, data[x]);
+        } 
+        //if object, recursive the data creating new nodes until the data is all in a base type of string, number or empty
+        else {
+            //new empty node with just the name
+            currentNode.descendents[x] = new TreeNode2(x, []);
+            //set a new pointer node
+            let tempNode = currentNode.descendents[x];
+            //loop over the object data pointing to temp node
+            convertData(tempNode, data[x]);
+        }
+
+    })
+
+    console.log(root);
 }
 
-class TreeNode2 {
-    constructor(name, data) {
-        this.name = name;
-        this.data = data;
-        this.descendents = [];
-    }
-}
 
-function convertJSONToTree(jsondata) {
-
-}
 
 //custom html component
 class TreeNode extends HTMLElement {
