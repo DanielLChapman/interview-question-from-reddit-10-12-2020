cc4dc9fd6a578750bf57b16dbf01d1677ee109c2
Daniel Chapman <danielc@my.ccsu.edu>
2020-10-12 15:15:32 -0400

    Set up CSS Page and initial custom html component

--- main.js
+++ main.js
@@ -1,4 +1,45 @@
+//grabs the url from the input
 function grabUrl() {
     let urlInput = document.getElementById('url');
     console.log(urlInput.value);
 }
+
+//custom html component
+class TreeNode extends HTMLElement {
+
+    //check if the component is expanded
+    get expand() {
+        return this.hasAttribute('expand');
+    }
+
+    //set the expand option for component
+    set expand(val) {
+        if(val) {
+            this.setAttribute('expand', '')
+        } else {
+            this.removeAttribute('expand');
+        }
+    }
+
+    //switch the expand position by checking if it already exists
+    switchExpand() {
+        if(this.hasAttribute('expand')) {
+            this.removeAttribute('expand');
+        } else {
+            this.setAttribute('expand', '')
+        }
+    } 
+
+    //constructor to add event listener click to switch the expansion
+    constructor() {
+        super();
+
+        this.addEventListener('click', e => {
+            this.switchExpand();
+        })
+    }
+}
+
+
+//define the node
+customElements.define('tree-node', TreeNode);
